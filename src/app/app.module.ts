e31de399990ca06b5ec0898eb7739bff0e63d66b
mauriander <andermattenmauricio@gmail.com>
2022-03-27 10:39:21 -0300

    commit prueba 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercadeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
